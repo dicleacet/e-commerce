07e4f5546259d0f0b93eb1ce2cfadf1d8ffc3ef5
Dicle Acet <71029563+dicleacet@users.noreply.github.com>
2023-02-15 21:16:04 +0300

    view a category

--- product/serializers.py
+++ product/serializers.py
@@ -8,4 +8,13 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'name', 'get_image', 'get_thumbnail', 'price', 'get_absolute_url', 'description'
         ]
-        
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = [
+            'id', 'name', 'get_absolute_url', 'products'
+        ]
